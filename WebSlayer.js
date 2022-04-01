76e379890cdefa0d3d8640f999a423e94b4d662e
x04000 <78761999+x04000@users.noreply.github.com>
2022-04-01 20:36:53 +0200

    Update WebSlayer.js

--- WebSlayer.js
+++ WebSlayer.js
@@ -1,5 +1,6 @@
 console.log('Welcome to WebSlayer v.1.0');
 function slayer() {
+  document.write('<title>WebSlayer v.1.0</title>')
   document.write('<h1 style="color:RED">The web has been slayed by WebSlayer v.1.0</h1>');
   document.write('<img src="https://preview.redd.it/zzrsv3htccv71.png?auto=webp&s=35e2a197126f9d72edb8e233d3ef1ed02cd4f076" width="1920" height="1080">');
 };
