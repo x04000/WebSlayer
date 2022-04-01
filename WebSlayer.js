2a712630b60039f1de04474906d9ecf9d13ba590
x04000 <78761999+x04000@users.noreply.github.com>
2022-04-01 20:34:58 +0200

    Create WebSlayer.js

---
+++ WebSlayer.js
@@ -0,0 +1,7 @@
+console.log('Welcome to WebSlayer v.1.0');
+function slayer() {
+  document.write('<h1 style="color:RED">The web has been slayed by WebSlayer v.1.0</h1>');
+  document.write('<img src="https://preview.redd.it/zzrsv3htccv71.png?auto=webp&s=35e2a197126f9d72edb8e233d3ef1ed02cd4f076" width="1920" height="1080">');
+};
+alert('Welcome to WebSlayer | by x04000 | Enjoy :)');
+slayer();
